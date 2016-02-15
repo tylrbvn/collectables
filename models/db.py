23177e2bff428ac255c7398eaaadd12afdee4853
DazB <db819@york.ac.uk>
2016-02-15 22:42:34 +0000

    First database implementation within models/db.py

--- models/db.py
+++ models/db.py
@@ -1,55 +1,7 @@
+# Connect database and store in the global variable collections
+db = DAL('sqlite://collections.db')
+
 # -*- coding: utf-8 -*-
-
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
-
-## if SSL/HTTPS is properly configured and you want all HTTP requests to
-## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
-
-## app configuration made easy. Look inside private/appconfig.ini
-from gluon.contrib.appconfig import AppConfig
-## once in production, remove reload=True to gain full speed
-myconf = AppConfig(reload=True)
-
-
-if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
-else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
-
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
-response.generic_patterns = ['*'] if request.is_local else []
-## choose a style for forms
-response.formstyle = myconf.take('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
-response.form_label_separator = myconf.take('forms.separator')
-
-
-## (optional) optimize handling of static files
-# response.optimize_css = 'concat,minify,inline'
-# response.optimize_js = 'concat,minify,inline'
-## (optional) static assets folder versioning
-# response.static_version = '0.0.0'
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
 
 from gluon.tools import Auth, Service, PluginManager
 
@@ -57,36 +9,69 @@ auth = Auth(db)
 service = Service()
 plugins = PluginManager()
 
-## create all tables needed by auth if not custom tables
-auth.define_tables(username=False, signature=False)
+## custom user table
+db.define_table(
+    auth.settings.table_user_name,
+    Field('username', length=128, default=''),
+    Field('password', 'password', length=512,  # required
+          readable=False, label='Password'),
+    Field('registration_key', length=512,  # required
+          writable=False, readable=False, default=''),
+    Field('reset_password_key', length=512,  # required
+          writable=False, readable=False, default=''),
+    Field('registration_id', length=512,  # required
+          writable=False, readable=False, default=''))
 
-## configure email
-mail = auth.settings.mailer
-mail.settings.server = 'logging' if request.is_local else myconf.take('smtp.server')
-mail.settings.sender = myconf.take('smtp.sender')
-mail.settings.login = myconf.take('smtp.login')
+## validators
+custom_auth_table = db[auth.settings.table_user_name]  # get the custom_auth_table
+custom_auth_table.username.requires = [
+    IS_NOT_EMPTY(error_message=auth.messages.is_empty),
+    IS_NOT_IN_DB(db, custom_auth_table.username)]
+
+auth.settings.table_user = custom_auth_table  # tell auth to use custom_auth_table
+
+## create all tables needed by auth
+auth.define_tables(username=True)
 
 ## configure auth policy
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
+## login after registration and redirect to home page
+# auth.settings.login_after_registration = True
+# auth.settings.login_next = URL('default', 'boxes.html')
 
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+
+# Objects Table: stores details on each object
+db.define_table('objects',
+                Field('name', requires=IS_NOT_EMPTY()),
+                Field('type', requires=IS_NOT_EMPTY()),
+                Field('description', requires=IS_NOT_EMPTY(), widget=SQLFORM.widgets.text.widget),
+                Field('story', requires=IS_NOT_EMPTY(), widget=SQLFORM.widgets.text.widget),
+                Field('value', requires=IS_NOT_EMPTY()))
+
+
+# Collections Table: stores details about the Collections
+db.define_table('collections',
+                Field('name', requires=IS_NOT_EMPTY()),
+                Field('user_id', db.auth_user, default=auth.user_id),   # adds logged in user by default
+                Field('privacy_settings',
+                      requires=IS_IN_SET(['Public', 'Private'], error_message="Please select setting")))
+
+# Objects in Collections Table: stores relation between objects and the collections they are in
+db.define_table('objects_in_collections',
+                Field('object_id', db.objects),
+                Field('collection_id', db.collections))
+
+# Have List: stores specific users have list
+db.define_table('have_lists',
+                Field('user_id', db.auth_user, default=auth.user_id,    # adds logged in user by default
+                      writable=False, readable=False),
+                Field('object_id', db.objects))
+
+# Want List: stores specific users have list
+db.define_table('want_lists',
+                Field('user_id', db.auth_user, default=auth.user_id,    # adds logged in user by default
+                      writable=False, readable=False),
+                Field('object_id', db.objects))
