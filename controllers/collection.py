516404e79bd8abf30da0dd9b1b524fad323281c1
Tyler Bevan <tdb506@york.ac.uk>
2016-02-26 17:00:25 +0000

    Fix small errors

--- controllers/collection.py
+++ controllers/collection.py
@@ -44,7 +44,7 @@ def view():
         if auth.is_logged_in():
             colls = db((db.collections.id == collection_id) & ((db.collections.privacy == 'Public') | (db.collections.user_id == auth.user.id)) & (db.collections.user_id == db.auth_user.id)).select()
         else:
-            colls = db((db.collections.id == collection_id) & (db.collections.privacy == 'Private') & (db.collections.owner_id == db.auth_user.id)).select()
+            colls = db((db.collections.id == collection_id) & (db.collections.privacy == 'Public') & (db.collections.user_id == db.auth_user.id)).select()
         if len(colls)>0:
             objects = db((db.objects_in_collections.collection_id == collection_id) & (db.objects_in_collections.object_id == db.objects.id) & (db.objects.user_id == db.auth_user.id)).select()
             return dict(collections = colls, objects = objects)
