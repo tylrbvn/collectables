5f092d97bf761228993d65c80c5e831e8f22fbe4
Tyler Bevan <tdb506@york.ac.uk>
2016-02-26 15:06:19 +0000

    Add basic SQLForm for collection creation

---
+++ controllers/collection.py
@@ -0,0 +1,9 @@
+@auth.requires_login()
+def new():
+    db.collections.user_id.readable = db.collections.user_id.writable = False
+    form = SQLFORM(db.collections)
+    if form.accepts(request.vars, session):
+        response.flash = 'New collection successfully created.'
+    elif form.errors:
+        response.flash = 'One or more of the entries is incorrect:'
+    return dict(form = form)
