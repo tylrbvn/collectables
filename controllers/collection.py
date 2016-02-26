7abcad84124d70447dce6236e4b90e345ef94c94
Tyler Bevan <tdb506@york.ac.uk>
2016-02-26 15:35:27 +0000

    Add page to view user's collections

--- controllers/collection.py
+++ controllers/collection.py
@@ -1,4 +1,13 @@
 @auth.requires_login()
+def index():
+    """Displays users collection"""
+    #Find users pubic boxes
+    public = db((db.collections.user_id==auth.user.id) & (db.collections.privacy == 'Public')).select()
+    #Find users private boxes
+    private = db((db.collections.user_id==auth.user.id) & (db.collections.privacy == 'Private')).select()
+    return dict(public_collections = public, private_collections = private)
+
+@auth.requires_login()
 def new():
     db.collections.user_id.readable = db.collections.user_id.writable = False
     form = SQLFORM(db.collections)
