0f9862ed9ecc61f20302552fa2451a82148704bc
Tyler Bevan <tdb506@york.ac.uk>
2016-03-02 19:04:25 +0000

    Improve draft homepage

--- controllers/default.py
+++ controllers/default.py
@@ -9,16 +9,11 @@
 #########################################################################
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    """response.flash = T("Hello World")"""
-    return dict()
-
+    #Get the 8 largest public collections
+    count = db.objects_in_collections.collection_id.count()  #What we are counting, the number of objects in each collection
+    #Perform the joint query and get info about owner
+    largest = db((db.collections.id==db.objects_in_collections.collection_id) & (db.collections.privacy == 'Public') & (db.collections.user_id == db.auth_user.id)).select(db.collections.name, db.collections.id, db.auth_user.username, count, groupby=db.objects_in_collections.collection_id, orderby=~count, limitby=(0,8))
+    return dict(largest = largest)
 
 def user():
     """
