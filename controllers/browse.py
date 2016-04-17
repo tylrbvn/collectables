fe55ddab5468311748756718605e05084e400542
Tyler Bevan <tdb506@york.ac.uk>
2016-04-17 21:51:34 +0100

    Add browsing off have and want lists

--- controllers/browse.py
+++ controllers/browse.py
@@ -1,12 +1,17 @@
 def collection():
-    #Get the 8 largest public collections
     count = db.objects_in_collections.collection_id.count()  #What we are counting, the number of objects in each collection
     #Perform the joint query and get info about owner
-    collections = db((db.collections.id==db.objects_in_collections.collection_id) & (db.collections.privacy == 'Public') & (db.collections.user_id == db.auth_user.id)).select(db.collections.name, db.collections.id, db.auth_user.username, count, groupby=db.objects_in_collections.collection_id, orderby=~count, limitby=(0,8))
+    collections = db((db.collections.id==db.objects_in_collections.collection_id) & (db.collections.privacy == 'Public') & (db.collections.user_id == db.auth_user.id)).select(db.collections.name, db.collections.id, db.auth_user.username, count, groupby=db.objects_in_collections.collection_id, orderby=~count)
     return dict(collections = collections)
 
 def have():
-    return dict()
+    count = db.have_lists.user_id.count()  #What we are counting, the number of objects in have list
+    #Perform the joint query and get info about owner
+    have_lists = db((db.have_lists.user_id == db.auth_user.id)).select(db.auth_user.id, db.auth_user.username, count, groupby=db.have_lists.user_id, orderby=~count)
+    return dict(lists = have_lists)
 
 def want():
-    return dict()
+    count = db.want_lists.user_id.count()  #What we are counting, the number of objects in want list
+    #Perform the joint query and get info about owner
+    want_lists = db((db.want_lists.user_id == db.auth_user.id)).select(db.auth_user.id, db.auth_user.username, count, groupby=db.want_lists.user_id, orderby=~count)
+    return dict(lists = want_lists)
